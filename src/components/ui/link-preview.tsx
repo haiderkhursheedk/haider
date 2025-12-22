570ab372e3a3f478b1f1a5e07ba0311f1e1973f7
Abdul Rehman Khan <arktechsolution7@gmail.com>
2025-12-22 23:09:04 +0530

    link

--- src/components/ui/link-preview.tsx
+++ src/components/ui/link-preview.tsx
@@ -64,7 +64,7 @@ export const LinkPreview = ({
   const translateX = useSpring(x, springConfig);
 
   const handleMouseMove = (event: React.MouseEvent<HTMLElement>) => {
-    const targetRect = event.target.getBoundingClientRect();
+    const targetRect = event.currentTarget.getBoundingClientRect();
     const eventOffsetX = event.clientX - targetRect.left;
     const offsetFromCenter = (eventOffsetX - targetRect.width / 2) / 2; // Reduce the effect to make it subtle
     x.set(offsetFromCenter);
@@ -124,7 +124,7 @@ export const LinkPreview = ({
                   x: translateX,
                 }}
               >
-                <a
+                
                   href={url}
                   className="block p-1 bg-white border-2 border-transparent shadow rounded-xl hover:border-neutral-200 dark:hover:border-neutral-800"
                   style={{ fontSize: 0 }}
